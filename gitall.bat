1cc0d4317d6bdf90be45c0d87c60d771e868b5d5
Chris Broderick <chris.broderick@verizon.net>
2025-02-14 17:08:50 -0500

    Commit

--- gitall.bat
+++ gitall.bat
@@ -1,2 +1,7 @@
-git add .
-git commit -m "Commit"
+echo "# BowlCupTng" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/JackBroderick/BowlCupTng.git
+git push -u origin main
